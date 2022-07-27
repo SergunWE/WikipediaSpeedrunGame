b7dfa110fc8c947cf28dbd41e98a0552e0904707
Sergei Shubenkov <sergunwe152@gmail.com>
2022-07-27 16:34:13 +0300

    Локализация

---
+++ WikipediaSpeedrunGame/WikipediaSpeedrunGame.UWP/Localize.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(WikipediaSpeedrunGame.UWP.Localize))]
+namespace WikipediaSpeedrunGame.UWP
+{
+    public class Localize : ILocalize
+    {
+        public CultureInfo GetCurrentCultureInfo()
+        {
+            return CultureInfo.CurrentUICulture;
+        }
+    }
+}
---
+++ WikipediaSpeedrunGame/WikipediaSpeedrunGame.iOS/Localize.cs
@@ -0,0 +1,36 @@
+﻿using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using UIKit;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(WikipediaSpeedrunGame.iOS.Localize))]
+namespace WikipediaSpeedrunGame.iOS
+{
+    public class Localize : ILocalize
+    {
+        public CultureInfo GetCurrentCultureInfo()
+        {
+            var netLanguage = "en";
+            var prefLanguage = "en-US";
+            if (NSLocale.PreferredLanguages.Length > 0)
+            {
+                var pref = NSLocale.PreferredLanguages[0];
+                netLanguage = pref.Replace("_", "-");
+            }
+            CultureInfo ci = null;
+            try
+            {
+                ci = new CultureInfo(netLanguage);
+            }
+            catch
+            {
+                ci = new CultureInfo(prefLanguage);
+            }
+            return ci;
+        }
+    }
+}
